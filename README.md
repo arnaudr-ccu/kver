f61bbd5e1d4cc8a75f335145051800d2c98976bb
Arnaud Rebillout <arnaud.rebillout@collabora.com>
2019-12-11 13:18:04 +0700

    Implement the -release flag
    Signed-off-by: Arnaud Rebillout <arnaud.rebillout@collabora.com>

--- README.md
+++ README.md
@@ -1,15 +1,37 @@
 kver (aka kernel version) reads a file and looks for a string that looks like a
 version, something like `\d\d*\.\d\d*\.\d\d*\S*`. The first match is returned.
 
-Use-case:
-
-    $ ./kver -kernel /boot/vmlinuz-linux
+    $ kver -kernel /boot/vmlinuz-linux
     5.3.0-2-amd64
     $ ls /usr/lib/modules
     5.3.0-2-amd64
 
     # !!! amazing, it matches !!!
-    # now scripts know which modules
-    # and which kernels go together
+    # knowing a particular kernel file,
+    # scripts can find the corresponding modules
+
+    $ for f in /boot/vmlinuz-*; do echo "$f -> $(kver -kernel $f)"; done
+    /boot/vmlinuz-linux -> 5.4.2-arch1-1
+    /boot/vmlinuz-linux-hardened -> 5.3.15.a-1-hardened
+    /boot/vmlinuz-linux-lts -> 4.19.88-1-lts
+    /boot/vmlinuz-linux-zen -> 5.4.2-zen1-1-zen
+
+kver can also take a string, and then scan installed kernels for this string.
+It returns the path of the first kernel where the string was found.
+
+    $ ls /usr/lib/modules
+    5.3.0-2-amd64
+    $ kver -release 5.3.0-2-amd64
+    /boot/vmlinuz-linux
+
+    # !!! amazing !!!
+    # knowing a particular kernel release,
+    # scripts can find the corresponding kernel
+
+    $ for f in /lib/modules/[0-9]*; do echo "$f -> $(kver -release $(basename $f))"; done
+    /lib/modules/4.19.88-1-lts -> /boot/vmlinuz-linux-lts
+    /lib/modules/5.3.15.a-1-hardened -> /boot/vmlinuz-linux-hardened
+    /lib/modules/5.4.2-arch1-1 -> /boot/vmlinuz-linux
+    /lib/modules/5.4.2-zen1-1-zen -> /boot/vmlinuz-linux-zen
 
 See: https://stackoverflow.com/q/3180029/776208
--- main.go
+++ main.go
@@ -4,13 +4,18 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
+// Find a release-like string in a file, using a regexp.
+// Returns the first match.
 func find_release_string(path string) (string, error) {
 	buf, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -25,6 +30,35 @@ func find_release_string(path string) (string, error) {
 	// cleanup any trailing non-printable characters
 	match = regexp.MustCompile(`[[:ascii:]]*`).Find(match)
 	return string(match), nil
+}
+
+// Find a static string in a file.
+// Returns true if found.
+func find_static_string(path string, str string) (bool, error) {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return false, err
+	}
+
+	return bytes.Contains(buf, []byte(str)), nil
+}
+
+// List the files /boot/vmlinuz-*.
+// Returns the files found.
+func list_vmlinuzs() ([]string, error) {
+	files, err := ioutil.ReadDir("/boot")
+	if err != nil {
+		return nil, err
+	}
+
+	kernels := []string{}
+	for _, f := range files {
+		if !f.IsDir() && strings.HasPrefix(f.Name(), "vmlinuz-") {
+			kernels = append(kernels, filepath.Join("/boot", f.Name()))
+		}
+	}
+
+	return kernels, nil
 }
 
 func main() {
@@ -46,6 +80,41 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Println(ret)
+
+	} else if release != "" {
+		if ! strings.HasSuffix(release, " ") {
+			release += " "
+		}
+
+		kernels, err := list_vmlinuzs()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error:", err)
+			os.Exit(1)
+		}
+
+		kernel_found := ""
+		for i := len(kernels) - 1; i >= 0; i-- {
+			//fmt.Fprintln(os.Stderr, "Scanning", filename, "...")
+			found, err := find_static_string(kernels[i], release)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error while scanning:", err)
+				continue
+			}
+			if ! found {
+				continue
+			}
+
+			kernel_found = kernels[i]
+			break
+		}
+
+		if kernel_found == "" {
+			fmt.Fprintln(os.Stderr, "No kernel found")
+			os.Exit(1)
+		}
+
+		fmt.Println(kernel_found)
+
 	} else {
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 		flag.PrintDefaults()
